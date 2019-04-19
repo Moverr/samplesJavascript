52039a0153af90af43f526540711f221ec6de924
Muyinda Rogers <moverr@gmail.com>
2019-04-19 11:39:29 +0300

    :Debuggin :

--- OddOcurrencesInArray.js
+++ OddOcurrencesInArray.js
@@ -3,17 +3,58 @@ function solution(A) {
     if (A instanceof Array && A.length > 0) {
 
 
-        console.log(A);
-
-
-        let count = 0;
-        while (count < A.length) {
-
-            count = count + 1;
-        }
+        cycleArray(A);
     }
+}
+
+
+function cycleArray(A) {
+   
+    let ArrayIndexes = Array();
+    let count = 0;
+    while (count < A.length) {
+   
+        if (ArrayIndexes.length > 0) {
+            for (let index = 0; index < ArrayIndexes.length; index++) {
+                if (A[ArrayIndexes[index]] == A[count]) {
+
+                    let newArray = Array();
+                    for(let index2 = 0; index2 < A.length; index2 ++){
+                        if(index2 == ArrayIndexes[index] || index2 == count ){
+                            continue;
+                        }
+                        newArray.push(A[index2]);
+                    }
+
+                  
+                    
+                    console.log(newArray);
+
+                     
+                    return cycleArray(newArray); 
+                }else{
+                    console.log("----PINK------");
+                    console.log(A);
+                }
+            }
+        }
+        // console.log(count);
+
+        ArrayIndexes.push(count);
+        
+           
+        
+        count = count + 1;
+    }
+
+    console.log("Afressh");
+    console.log(A);
+    
+
+
 }
 
 
 let A = [9, 3, 9, 3, 9, 7, 9];
 solution(A);
+
